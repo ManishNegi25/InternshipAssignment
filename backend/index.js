1b2a5b6f29de61635ddfee31690dd52e2339702a
MANISHNEGI <147482023+ManishNegi25@users.noreply.github.com>
2024-09-06 14:59:45 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,11 +10,13 @@ const PORT = 5000;
 
 
 // Allow requests from your specific frontend URL
-app.use(cors({
-  origin: 'https://internship-assignment-frontendlink.vercel.app', // Your frontend URL
-  methods: 'GET,POST,PUT,DELETE,OPTIONS', // Allowed methods
-  allowedHeaders: 'Content-Type, Authorization' // Allowed headers
-}));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://internship-assignment-frontendlink.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 
 
 app.use(bodyParser.json());
